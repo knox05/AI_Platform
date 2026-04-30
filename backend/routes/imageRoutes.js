657f4fb36386f145926e1b58d096372df06b36a1
Vyankatesh Punnal <126763392+knox05@users.noreply.github.com>
2026-04-30 12:24:02 +0530

    Delete backend directory

--- backend/routes/imageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { createImage } = require("../controllers/imageController");
-
-router.post("/generate-image", createImage);
-
-module.exports = router;
